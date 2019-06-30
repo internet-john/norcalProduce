58f13d5ebcb7cabe2dec645fbff87757c29cbe1d
etzJohn <holman.jx@gmail.com>
2019-06-30 16:17:07 -0700

    commit one

---
+++ src/tileData.js
@@ -0,0 +1,64 @@
+const tileData = [
+  {
+    img: '/static/images/grid-list/breakfast.jpg',
+    title: 'Stone fruit (Apricots, nectarines, peaches, plums, pluots)',
+    season: 'Summer'
+  },
+  {
+    img: '/static/images/grid-list/burgers.jpg',
+    title: 'Artichokes',
+    season: 'Summer'
+  },
+  {
+    img: '/static/images/grid-list/camera.jpg',
+    title: 'Avocados',
+    season: 'Summer'
+  },
+  {
+    img: '/static/images/grid-list/morning.jpg',
+    title: 'Berries (blueberries, boysenberries, raspberries, strawberries)',
+    season: 'Summer'
+  },
+  {
+    img: '/static/images/grid-list/hats.jpg',
+    title: 'Broccoli',
+    season: 'Summer'
+  },
+  {
+    img: '/static/images/grid-list/honey.jpg',
+    title: 'Cauliflower',
+    season: 'Summer'
+  },
+  {
+    img: '/static/images/grid-list/vegetables.jpg',
+    title: 'Figs',
+    season: 'Summer'
+  },
+  {
+    img: '/static/images/grid-list/plant.jpg',
+    title: 'Green beans',
+    season: 'Summer'
+  },
+  {
+    img: '/static/images/grid-list/mushroom.jpg',
+    title: 'Melons',
+    season: 'Summer'
+  },
+  {
+    img: '/static/images/grid-list/olive.jpg',
+    title: 'Rhubarb',
+    season: 'Summer'
+  },
+  {
+    img: '/static/images/grid-list/star.jpg',
+    title: 'Snap peas',
+    season: 'Summer'
+  },
+  {
+    img: '/static/images/grid-list/bike.jpg',
+    title: 'Summer squash',
+    season: 'Summer'
+  },
+];
+
+export default tileData;
